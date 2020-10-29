037515a9f15cf3e210d4a2fffbde3bc72f6fc97b
Cesar Nuñez <alejoo2310@gmail.com>
2020-10-29 04:17:10 -0500

    Login done

--- Consultas/validacionUsuario.php
+++ Consultas/validacionUsuario.php
@@ -4,12 +4,17 @@ include_once '../config.php';
 include_once '../utils.php';
 
 $dbConn = connect($db);    
-    $sql = $dbConn->prepare("SELECT rol, ID_usuario FROM usuario WHERE correo = :correo and password = :password");
+    $sql = $dbConn->prepare("SELECT rol, ID_usuario, nombre FROM usuario WHERE correo = :correo and password = :password");
     $sql->bindValue(':correo', $_POST['correo']);
     $sql->bindValue(':password', $_POST['password']);
     $sql->execute();
     header("HTTP/1.1 200 OK");
-    echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+    $res = $sql->fetch(PDO::FETCH_ASSOC);
+    if ($res) {
+        echo '{"code":"0", "msg":"done", "data":'.json_encode(json_encode($res)).'}';
+    } else {
+        echo '{"code":"-1", "msg":"Correo y contraseña incorrecta", "data":"'.$res.'"}';
+    }   
     exit();
 
     header("HTTP/1.1 400 Bad Request");
